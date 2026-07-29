83ccae528afc206b0936fc33000cd491eb8c434d
Luka Šulc <lukasulc0@gmail.com>
2026-07-29 21:00:23 +0200

    Update dependencies

--- url-to-mealie/src/templates/templates.py
+++ url-to-mealie/src/templates/templates.py
@@ -15,8 +15,9 @@ def get_homepage(request: Request, url: str, token: str | None) -> Any:
     status_class = "success" if token and url else "danger"
 
     return templates.TemplateResponse(
-        "homepage.html",
-        {
+        request=request,
+        name="homepage.html",
+        context={
             "request": request,
             "url": url,
             "mealie_status": mealie_status,
@@ -28,7 +29,9 @@ def get_homepage(request: Request, url: str, token: str | None) -> Any:
 def get_exception_page(request: Request, error_message: str) -> Any:
     """Generate the exception page HTML."""
     return templates.TemplateResponse(
-        "exception.html", {"request": request, "error_message": error_message}
+        request=request,
+        name="exception.html",
+        context={"request": request, "error_message": error_message},
     )
 
 
@@ -37,8 +40,9 @@ def get_success_page(
 ) -> Any:
     """Generate the success page HTML."""
     return templates.TemplateResponse(
-        "success.html",
-        {
+        request=request,
+        name="success.html",
+        context={
             "request": request,
             "recipe_url": recipe_url,
             "recipe_name": recipe_name,
@@ -50,7 +54,9 @@ def get_success_page(
 def get_error_page(request: Request, error_message: str, url: str) -> Any:
     """Generate the error page HTML."""
     return templates.TemplateResponse(
-        "error.html", {"request": request, "error_message": error_message, "url": url}
+        request=request,
+        name="error.html",
+        context={"request": request, "error_message": error_message, "url": url},
     )
 
 
@@ -59,14 +65,20 @@ def get_instagram_error(
 ) -> Any:
     """Generate the Instagram error page HTML."""
     return templates.TemplateResponse(
-        "instagram_error.html",
-        {"request": request, "errors": errors, "suggestions": suggestions},
+        request=request,
+        name="instagram_error.html",
+        context={"request": request, "errors": errors, "suggestions": suggestions},
     )
 
 
 def get_status_page(request: Request, queue_status: Dict[str, Any], mealie_url) -> Any:
     """Generate the status page HTML."""
     return templates.TemplateResponse(
-        "status.html",
-        {"request": request, "queue_status": queue_status, "mealie_url": mealie_url},
+        request=request,
+        name="status.html",
+        context={
+            "request": request,
+            "queue_status": queue_status,
+            "mealie_url": mealie_url,
+        },
     )
